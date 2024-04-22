6e246418c4c99b1f2b2f0a197cef8120be0623b0
kaluu554 <143027760+kaluu554@users.noreply.github.com>
2024-04-22 12:25:24 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -3,10 +3,7 @@ public class RandomLetterChooser extends RandomStringChooser
 	
 	public RandomLetterChooser(String str)
 	{ 
-		/* to be implemented in part (b) */
-		public RandomLetterChooser(String str){
-			super(getSingleLetter(str));
-		}
+		super(getSingleLetter(str));
 	}
 	
 	
